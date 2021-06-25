58f8edd5b67e7c3ea221f65f18036d21506f1d56
Pedro Paulo <Pedrinho_fsf@hotmail.com>
2021-06-24 23:05:16 -0300

    :sparkles: Adicionado a cor de fundo

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,24 @@ export default {
 </script>
 
 <style>
+  :root {
+    /* Primary */
+    --very-dark-blue: hsl(233, 47%, 7%);
+    --dar-desaturated-blue: hsl(244, 38%, 16%);
+    --soft-violet: hsl(277, 64%, 61%);
+
+    /* Neutral */
+    --white: hsl(0, 0%, 100%);
+    --slightly-transparent-white-main-paragraph: hsla(0, 0%, 100%, 0.75);
+    --slightly-transparent-white-stat-headings: hsla(0, 0%, 100%, 0.6)
+  }
+
   #app {
     margin: 0;
     padding: 0; 
+  }
+
+  body {
+    background-color: var(--very-dark-blue);
   }
 </style>
---
+++ src/components/CardMain.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="CardMain">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CardMain",
+        component: {
+
+        },
+    }
+</script>
+
+<style scoped>
+    div#CardMain {
+        background-color: var(--dark-desaturated-blue);
+    }
+</style>
